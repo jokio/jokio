c767cc3df29cf155a47150980b0557ebda6a4ae4
Ezeki Zibzibadze <ez@jok.io>
2018-01-27 12:22:58 +0400

    debug update

--- src/_debug.ts
+++ src/_debug.ts
@@ -1,3 +1,22 @@
-import uniqueId from "./uniqueId";
+import compose from "./compose";
+import log from "./log";
 
-console.log('debug works', uniqueId())
+const process1 = (props) => (state, context) => {
+	return { ...state, info: state.info + ' hello' };
+}
+
+const process2 = (props) => (state, context) => {
+	return Promise.resolve({ ...state, info: state.info + ' world' });
+}
+
+const initialState = {
+	info: '[Cool App]'
+}
+
+const run = compose(
+	process1({}),
+	process2({}),
+	log(),
+)
+
+run(initialState);
